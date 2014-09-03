71dea5ce78a17d5dd61cc3b227a86b43a904898f
Evgeny Slobodskoy <eslobodskoy@etargeting.ru>
2014-09-03 13:31:52 +0400

    static wsdl added, url to wsdl changed

---
+++ src/main/java/ru/slobevg/config/WebConfig.java
@@ -0,0 +1,12 @@
+package ru.slobevg.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"ru.slobevg.web.controller"})
+public class WebConfig {
+
+}
